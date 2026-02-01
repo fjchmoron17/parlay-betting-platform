dca6cd37e3e21ad1fa7b13ab636b5e2cad28d8e3
fjchmoron17 <fjchmoron@gmail.com>
2026-02-01 17:32:51 +0100

    fix: allow x-admin-token header in CORS

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-admin-token']
 }));
 
 app.use(express.json());
